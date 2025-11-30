a174cb136cae10c2ff892f204753503e585bb30f
gemmines2 <yousefzubair03@gmail.com>
2025-11-30 00:00:53 -0800

    Update products.js

--- products.js
+++ products.js
@@ -55,5 +55,39 @@ const products = [
     price: 200.00,
     description: "Soothing Aquamarine – Stone of tranquility, clarity & courage."
   }
- 
+ {
+    id: "umbalitegarnet_001",
+    name: "Umbalite Garnet",
+    image: "images/umbalitegarnet.jpg",
+    price: 400.00,
+    description: "Rare Umbalite Garnet – Deep pinkish-red, symbolizing love, energy & passion."
+  },
+  {
+    id: "almandinegarnet_002",
+    name: "Almandine Garnet",
+    image: "images/Almandine-ring.jpg", 
+    price: 150.00,
+    description: "Deep red Almandine Garnet – Stone of strength, energy, and balance."
+  },
+  {
+    id: "tourmaline_003",
+    name: "Tourmaline",
+    image: "images/green-tourmaline.jpg",
+    price: 180.00,
+    description: "Natural Tourmaline gemstone – Protection, positivity & emotional healing."
+  },
+  {
+    id: "mixdgem_004",
+    name: "Mixed Gemstones",
+    image: "images/mixed-gemstones.jpg",
+    price: 600.00,
+    description: "Premium Mixed Natural Gemstones Collection – Perfect for collectors."
+  },
+  {
+    id: "greenjasper_005",
+    name: "Green Jasper",
+    image: "images/green-jasper.jpg",
+    price: 90.00,
+    description: "Green Jasper – Stone of harmony, balance, and natural protection."
+  },
 ];
