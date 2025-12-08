ba073b3a4d3a5a60ba43d4b56a6aaa24a86bcf65
gemmines2 <yousefzubair03@gmail.com>
2025-12-07 23:08:43 -0800

    Update products.js

--- products.js
+++ products.js
@@ -31,7 +31,7 @@ const products = [
     name: "Peridot",
     description: "Fresh Peridot – Stone of compassion, harmony, and renewal.",
     image: "https://gemmines2.github.io/gemmines2-website/images/peridot.jpg",
-    price: 350.00,
+    price: 90.00,
     availability: "in stock",
     brand: "Gemmines2"
   },
