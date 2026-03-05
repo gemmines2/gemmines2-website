b7c2d0fbcdbb8a464007daa2f1b62706493697ac
gemmines2 <yousefzubair03@gmail.com>
2026-03-05 06:29:25 -0800

    Update products.js

--- products.js
+++ products.js
@@ -347,7 +347,7 @@ const products = [
   description: "Unheated Natural Mixed Sapphire Sri Lankan Origin gemstone.",
   price: 95.00,
   currency: "USD",
-  image: "https://gemmines2.github.io/gemmines2-website/images/bio-color-sapphire.jpg",
+  image: "https://gemmines2.github.io/gemmines2-website/images/bio-color-darksapphire.jpeg",
   brand: "Gemmines2",
   availability: "in stock"
   {
