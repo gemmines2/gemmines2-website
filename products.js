7330908421e208ae1245e9ac5c494b47b6ebb03f
gemmines2 <yousefzubair03@gmail.com>
2026-03-26 02:34:11 -0700

    Update products.js

--- products.js
+++ products.js
@@ -4,7 +4,7 @@ const products = [
     name: "Umbalite Garnet",
     description: "Rare Umbalite Garnet – Symbol of love, energy & passion.",
     image: "https://gemmines2.github.io/gemmines2-website/images/umbalitegarnet.jpg",
-    price: 400.00,
+    price: 200.00,
     availability: "in stock",
     brand: "Gemmines2"
   },
