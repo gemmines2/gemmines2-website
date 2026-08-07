28338b9434e10047f78ffabdd27d1eebbe019715
gemmines2 <yousefzubair03@gmail.com>
2026-08-06 23:03:21 -0700

    Update products-data.js

--- products-data.js
+++ products-data.js
@@ -1785,31 +1785,7 @@ const PRODUCTS = [
     "video": "images/gm-sap-1080v.mp4"
   },
 
-    {
-    "id": "gm-sap-1081-1",
-    "name": "Natural Heated Orange Sapphire 1.15 Carat Round Cut | Rare Orange Colour",
-    "shortName": "Sapphire",
-    "type": "cut",
-    "treatment": "heated",
-    "origin": "Sri Lanka",
-    "color": "Orange",
-    "shape": "Round Brilliant Cut",
-    "weight": 1.15,
-    "dimensions": "",
-    "clarity": "Eye Clean",
-    "certificate": "Available on Request",
-    "price": 80,
-    "pricePerCarat": 70,
-    "status": "active",
-    "rare": false,
-    "description": "Discover a beautiful Natural Heated Orange Sapphire weighing 1.15 carats, sourced from the famous gem deposits of Sri Lanka (Ceylon). Displaying a naturally rare orange colour, this sapphire has been enhanced only through traditional heat treatment, a permanent and widely accepted practice in the gemstone industry.",
-    "gemType": "Sapphire",
-    "images": [
-      "images/gm-sap-1081-1.jpg"
-    ],
-    "image": "images/gm-sap-1081-1.jpg",
-    "video": ""
-  },
+   
       {
     "id": "gm-sap-1025",
     "name": "Natural Blue Sapphire 1 Carat Heated Eye Clean Cushion Cut  Cut & Polished",
